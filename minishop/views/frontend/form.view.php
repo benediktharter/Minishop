8549a265f59ff4b87241283b5e0984d7c3dbd130
Moncho Varela <nakome@gmail.com>
2014-09-29 18:12:37 +0200

    Update form.view.php

--- minishop/views/frontend/form.view.php
+++ minishop/views/frontend/form.view.php
@@ -1,9 +1,10 @@
-<div class="col-md-6">
+
 <?php 
 
 echo
     Form::open(null ,array('id' =>'ms_formCheckout','class' => 'form')).
 
+'<div class="col-md-6">'.
 
   '<div class="form-group">'.
     Form::label('ms_name',__('Name *', 'minishop')).
@@ -35,45 +36,6 @@ echo
     '<small class="help-block">'.__('Write your Shipping Address','minishop').' 2</small>'.
   '</div>'.
 
-
- '<div class="form-group">'.
-    '<span id="items" class="simpleCart_quantity hide"></span></p>'.
-    '<span id="total" class="simpleCart_grandTotal hide"></span></p>'.
-  '</div>'.
-
-
-  '<p><b>'.__('Select paid method','minishop').':</b></p>'.
-  '<div class="radio">'.
-      '<label>'.
-        Form::radio('paidmethod','cashondelivery').'<span>'.__('Cash on delivery','minishop').'  (<small> '.Option::get('ms_cashontax').'</small>)</span>'.
-      '</label>'.
-  '</div>'.
-
-  '<div class="radio">'.
-        '<label>'.
-          Form::radio('paidmethod','paypal').'<span>'.__('Paypal','minishop').'</span>'.
-        '</label>'.
-    '</div>'.
-
-
-  '<div class="checkbox">'.
-      '<label>'.
-        Form::checkbox('imnotarobot').'<span style="color:#f55">'.__('Check if are not a robot *','minishop').'</span>'.
-      '</label>'.
-  '</div>'.
-        Form::hidden('ms_purchased').
-        Form::hidden('ms_total').
-        Form::hidden('csrf', Security::token()).Html::br().
-        Form::submit('ms_submit',__('Save', 'minishop'), array('class' => 'btn','onclick' => 'validate();')).
-
-
-
-
-'</div>'.
-
-'<div class="col-md-6">'.
-
-
   '<div class="form-group">'.
     Form::label('ms_locality',__('Locality','minishop')).
     Form::input('ms_locality','',array('class' => 'form-control')).
@@ -81,6 +43,10 @@ echo
  '</div>'.
 
 
+  
+'</div>'.
+
+'<div class="col-md-6">'.
 
   '<div class="form-group">'.
     Form::label('ms_country',__('Country','minishop')).
@@ -88,27 +54,60 @@ echo
     '<small class="help-block">'.__('Write your Country','minishop').'</small>'.
   '</div>'.
 
-
-
-
   '<div class="form-group">'.
     Form::label('ms_phone',__('Phone','minishop')).
     Form::input('ms_phone','',array('class'=>'form-control')).
     '<small class="help-block">'.__('Write your Contact Phone','minishop').'</small>'.
   '</div>'.
 
-
-
-
   '<div class="form-group">'.
     Form::label('ms_comment',__('Comments','minishop')).
     '<div class="controls">'.
-    Form::textarea('ms_comment','',array('rows'=>'5','class'=>'form-control')).
+    Form::textarea('ms_comment','',array('rows'=>'10','class'=>'form-control')).
     '<small class="help-block">'.__('Write a Shipping Details','minishop').'</small>'.
     '</div>
   </div>'.
 
+  '<div class="form-group">'.
+    '<span id="items" class="simpleCart_quantity hide"></span></p>'.
+    '<span id="total" class="simpleCart_grandTotal hide"></span></p>'.
+  '</div>'.
+
+ '</div>'.
+
+
+
+'<div class="col-md-12">'.
+  '<div class="well well-sm">'.
+
+    '<p><b>'.__('Select paid method','minishop').':</b></p>'.
+
+    '<div class="radio">'.
+        '<label>'.
+          Form::radio('paidmethod','cashondelivery').'<span>'.__('Cash on delivery','minishop').'  (<small> '.Option::get('ms_cashontax').'</small>)</span>'.
+        '</label>'.
+    '</div>'.
+
+    '<div class="radio">'.
+          '<label>'.
+            Form::radio('paidmethod','paypal').'<span>'.__('Paypal','minishop').'</span>'.
+          '</label>'.
+      '</div>'.
+    '<div class="checkbox">'.
+        '<label>'.
+          Form::checkbox('imnotarobot').'<span style="color:#f55">'.__('Check if are not a robot *','minishop').'</span>'.
+        '</label>'.
+    '</div>'.
+
+    Form::hidden('ms_purchased').
+    Form::hidden('ms_total').
+    Form::hidden('csrf', Security::token()).Html::br().
+    Form::submit('ms_submit',__('Save', 'minishop'), array('class' => 'btn','onclick' => 'validate();')).
+
+  '</div>'.
+'</div>'.
+
   Form::close().Html::br(2);
 ?>
-</div>
+
 
