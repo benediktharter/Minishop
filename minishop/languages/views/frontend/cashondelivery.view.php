b6f793fbf862c7b3ebc6439888b858413bba8976
Moncho Varela <nakome@gmail.com>
2014-09-29 20:36:12 +0200

    Clear folders

--- minishop/languages/views/frontend/cashondelivery.view.php
+++
@@ -1,2 +0,0 @@
-<p><?php echo '<p>'.Option::get('ms_befchk').'</p>'; ?></p>
-<p><a href="<?php echo Site::url(); ?>/shop" ><?php echo __('Back to the shop','minishop'); ?></a></p>
--- minishop/languages/views/frontend/categories.view.php
+++
@@ -1,9 +0,0 @@
-<?php if($tags > 0){
-  foreach($tags as $tag) { ?>
-    <a class="label label-primary" href="<?php echo $url.$tag;?>" ><?php echo $tag; ?> </a>
-<?php }
-	}else{
-		echo __('No tags yet','minishop');
-	}
-?>
-
