2e29451c0fdff1d6049c563f3706aff90a4289c0
Moncho Varela <nakome@gmail.com>
2014-09-29 17:34:55 +0200

    Update new_items.view.php

--- minishop/views/frontend/new_items.view.php
+++ minishop/views/frontend/new_items.view.php
@@ -1,24 +1,23 @@
 <div id="shopProducts">
-    <ul class="products clearfix">
-        <?php
-        $html = '';
-        if(count($items)>0) foreach ($items as $item) {
-            $html.='<li id="Themes-1" class="simpleCart_shelfItem product ef">
-            <div class="padding-prod">
-            <div class="ms_image">
-            <a href="'.Option::get('siteurl').'shop/item?id='.$item['id'].' " title="'.$item['title'].'">
-            <img class="item_tumb" src="'.Option::get('siteurl').DS.'public/shop/large/'.$item['image1'].'">
-            </a>
-            </div>
-            <h4>'.$item['title'].'</h4>
-            <p class="desc-price">'.__("Price", "minishop").':<span class="item_price">'.$item['price'].'</span></p>
-            </div><!--/.padding-prod -->
-            <div class="ms_buttons">
-            <a href="'.Option::get('siteurl').DS.'shop/item?id='.$item['id'].' "><i class="icon-eye-open"></i> '.__("View details", "minishop").'</a>
-            </div>
-            </li>';
-        }
-        echo $html;
-        ?>
-    </ul>
+<?php
+    $html = '';
+    if(count($items)>0) foreach ($items as $item) {
+        $html.= '<div id="'.$item['product'].'-'.$item['id'].'" class="col-md-4">
+                 <div class="thumbnail">
+                    <a href="'.Option::get('siteurl').DS.miniShop::$shop.DS.'item?id='.$item['id'].'" title="'.$item['title'].'" >
+                      <img  src="'.Option::get('siteurl').DS.'public/shop/large/'.$item['image1'].'">
+                    </a>
+                    <div class="caption clearfix">
+                      <h4>'.$item['title'].'</h4>
+                      <div class="btn btn-default btn-sm pull-left"  disabled="disabled"><b>Precio: </b><span> '.$item['price'].'</span></div>
+                      <a class="btn btn-default btn-sm pull-right" href="'.Option::get('siteurl').DS.miniShop::$shop.DS.'item?id='.$item['id'].'" >
+                        '.__("View details", "minishop").'
+                      </a>
+                      
+                    </div>
+              </div>
+            </div>';
+    }
+    echo $html;
+?>
 </div>
